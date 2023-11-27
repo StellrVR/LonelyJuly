958b9f50a90581741d9d344088b7a6f5bb18531a
Stellr <87259721+StellrVR@users.noreply.github.com>
2023-11-26 21:00:04 -0800

    Update js

---
+++ js/command.js
@@ -0,0 +1,48 @@
+
+var home = "https://lonelyjuly.online/main.html"
+
+help = [
+    '+========================================================================================+',
+    '| Commands   | Descriptions                                                              |',
+    '+========================================================================================+',
+    '| <span class=\"command\">clear</span>      | Clears the terminal log                                                   |',
+    '| <span class=\"command\">warnings</span>   | Shows a list of warnings for what you might see on this website           |',
+    '| <span class=\"command\">enter</span>      | Enter the website                                                         |',
+    '| <span class=\"command\">history</span>    | View command history                                                      |',
+    '| <span class=\"command\">help</span>       | Shows what you\'re looking at right now!                                   |',
+    `| <span class=\"command\">banner</span>     | Shows the banner                                                          |`,
+    `| <span class=\"command\">credits</span>    | Credits of this page                                                      |`,
+    '+========================================================================================+',
+]
+
+
+
+warnings = [
+    "<br>",
+    "<span class=\"command\">This website may include the following:</span>",
+    "<span class=\"color2\">- Flashing lights</span>",
+    "<span class=\"color2\">- Mismatched colors and graphics</span>",
+    "<span class=\"color2\">- Bright colors and graphics</span>",
+    "<span class=\"color2\">- Self-indulgence</span>",
+    "<br>",
+    "<span class=\"command\">If you\'re okay with that, type \"<span class=\"command\">enter</span>\" below.<span>",
+    "<br>",
+]
+
+banner = [
+    " _                      _            _       _       ",
+    "| |                    | |          | |     | |      ",
+    "| |     ___  _ __   ___| |_   _     | |_   _| |_   _ ",
+    "| |    / _ &#92;| '_ &#92; / _ &#92; | | | |_   | | | | | | | | |",
+    "| |___| (_) | | | |  __/ | |_| | |__| | |_| | | |_| |",
+    "|______&#92;___/|_| |_|&#92;___|_|&#92;__, |&#92;____/ &#92;__,_|_|&#92;__, |",
+    "                           __/ |                __/ |",
+    "                          |___/                |___/ ",
+    ]
+
+suggestion = [
+  "<p class>",
+  "<span class=\"color2\">Welcome to my personal website! This site is to show who I am, what I do, and what I plan to do. As well as some thoughts I have in mind.<br>For a list of available commands, type <span class=\"command\">help</span>.",
+  "</span>",
+  "</p>",
+]
