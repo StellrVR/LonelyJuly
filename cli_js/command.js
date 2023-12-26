003441d9690a9b1772ce6a44338934326b9ecaff
Stellr <87259721+StellrVR@users.noreply.github.com>
2023-12-26 10:19:51 -0800

    Update cli_js

---
+++ cli_js/command.js
@@ -0,0 +1,113 @@
+var home = "https://lonelyjuly.online/main.html"
+var ring = "https://vrring.neocities.org/"
+var journey = "https://journey.lonelyjuly.online"
+var guestbook = "https://lonelyjuly.123guestbook.com"
+var blog = "https://blog.lonelyjuly.online"
+var github = "https://github.com/StellrVR"
+var vrchat = "https://vrchat.com/home/user/usr_b35d613e-3896-4303-a120-4fbbff8ef108"
+var bluesky = "https://bsky.app/profile/stellr.bsky.social"
+var discord = "https://discord.com/users/257252196926750720"
+
+help = [
+    '+========================================================================================+',
+    '| Commands   | Descriptions                                                              |',
+    '+========================================================================================+',
+    `| <span class=\"command\">home</span>       | Home                                                                      |`,
+    `| <span class=\"command\">about</span>      | About                                                                     |`,
+    `| <span class=\"command\">my_journey</span> | My Journey                                                                |`,
+    `| <span class=\"command\">links</span>      | Links                                                                     |`,
+    `| <span class=\"command\">guestbook</span>  | Guestbook                                                                 |`,
+    `| <span class=\"command\">webring</span>    | VRRing                                                                    |`,
+    '+========================================================================================+',
+    '| Outsider Link Commands                                                                 |',
+    '+========================================================================================+',
+    `| <span class=\"command\">blog</span>       | Blog                                                                      |`,
+    `| <span class=\"command\">github</span>     | Github                                                                    |`,
+    `| <span class=\"command\">vrchat</span>     | VRChat Profile                                                            |`,
+    `| <span class=\"command\">bluesky</span>    | Bluesky                                                                   |`,
+    `| <span class=\"command\">email</span>      | Email Me                                                                  |`,
+    `| <span class=\"command\">discord</span>    | Discord                                                                   |`,
+    '+========================================================================================+',
+    '| Terminal Commands                                                                      |',
+    '+========================================================================================+',
+    '| <span class=\"command\">clear</span>      | Clears the terminal log                                                   |',
+    '| <span class=\"command\">history</span>    | View command history                                                      |',
+    '| <span class=\"command\">help</span>       | Shows what you\'re looking at right now!                                   |',
+    `| <span class=\"command\">banner</span>     | Shows the banner                                                          |`,
+    `| <span class=\"command\">credits</span>    | Credits of this page                                                      |`,
+    '+========================================================================================+',
+]
+
+home = [
+    '+========================================================================================+',
+    `|                                                                                        |`,
+    `|   Welcome to my website (CLI)!                                                         |`,
+    `|   This is where I show what I do, who I am, what I plan on doing,                      |`,
+    `|   and some other things that are on my mind.                                           |`,
+    `|                                                                                        |`,
+    `|                                                                                        |`,
+    `|   Blog Preview                                                                         |`,
+    `|   <a href=\"https://journey.lonelyjuly.online/the-beginning\">The Beginning</a>                                                                        |`,
+    `|   <i>11/27/2023</i>                                                                           |`,
+    `|                                                                                        |`,
+    `+========================================================================================+`,
+]
+  
+about = [
+    '+========================================================================================+',
+      `|                                                                                        |`,
+      `|   Welcome to the About section!                                                        |`,
+      `|                                                                                        |`,
+      `|   Age: 24                                                                              |`,
+      `|   Nationality: United States                                                           |`,
+      `|   Personality: INTJ-T                                                                  |`,
+      `|                                                                                        |`,
+      `|   One things you'll get know about me is that I love space. I've watched               |`,
+      `|   Interstellar 22 times as of today.                                                   |`,
+      `|   I'm also really passionate about learning American Sign Language (ASL)               |`,
+      `|   and Deaf culture. I'm currently a Deaf Studies and Interpreting student,             |`,
+      `|   which you can read about my journey <a href=\"https://journey.lonelyjuly.online\">here!</a>                                            |`,
+      `|                                                                                        |`,
+      `+========================================================================================+`,
+      `|                                                                                        |`,
+      `|   Games I'm currently playing:                                                         |`,
+      `|   - Baldur\'s Gate 3                                                                    |`,
+      `|   - Lethal Company                                                                     |`,
+      `|   - VRChat (~3,100h on Steam)                                                          |`,
+      `|   - The Binding of Isaac                                                               |`,
+      `|   - Risk of Rain (2 & Returns)                                                         |`,
+      `|                                                                                        |`,
+      `+========================================================================================+`,
+      `|                                                                                        |`,
+      `|   What I'm working on currently:                                                       |`,
+      `|   - Kudo, a DPS distribution community avatar                                          |`,
+      `|   - This website                                                                       |`,
+      `|   - Postalina, a Pythonic Discord bot for the DPS                                      |`,
+      `|                                                                                        |`,
+      `+========================================================================================+`,
+      `|                                                                                        |`,
+      `|   What I plan to do:                                                                   |`,
+      `|   - Add more pages to the main site                                                    |`,
+      `|   - Gradute college (2026, hopefully)                                                  |`,
+      `|   - Who knows...?                                                                      |`,
+      `|                                                                                        |`,
+      `+========================================================================================+`,
+]
+
+banner = [
+    " _                      _            _       _       ",
+    "| |                    | |          | |     | |      ",
+    "| |     ___  _ __   ___| |_   _     | |_   _| |_   _ ",
+    "| |    / _ &#92;| '_ &#92; / _ &#92; | | | |_   | | | | | | | | |",
+    "| |___| (_) | | | |  __/ | |_| | |__| | |_| | | |_| |",
+    "|______&#92;___/|_| |_|&#92;___|_|&#92;__, |&#92;____/ &#92;__,_|_|&#92;__, |",
+    "                           __/ |                __/ |",
+    "                          |___/                |___/ ",
+    ]
+
+suggestion = [
+  "<p class>",
+  "<span class=\"color2\">Welcome to the CLI version of the site! This version is just something I wanted to do for fun, but it also serves as a way for someone to enjoy the site without all the flashing lights and colors.<br>For a list of available commands, type <span class=\"command\">help</span>.",
+  "</span>",
+  "</p>",
+]
